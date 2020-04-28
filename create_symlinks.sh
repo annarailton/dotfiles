653b4c6c83f8614fe8eaa5f2e3eccd7521558580
annarailton <annarailton@github.com>
2020-04-28 14:32:15 +0100

    Update install script with KDE option

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -11,15 +11,15 @@ old_dir=~/.old_dotfiles
 # Delete existing dot files and folders
 #======================================
 mkdir $old_dir
-mv -v .bash_aliases > $old_dir 2> /dev/null
-mv -v .bashrc > $old_dir 2> /dev/null
-mv -v .gitconfig > $old_dir 2> /dev/null
-mv -v .inputrc > $old_dir 2> /dev/null
-mv -v .mdlrc > $old_dir 2> /dev/null
-mv -v .nanorc > $old_dir 2> /dev/null
-mv -v .pre-commit-config.yaml > $old_dir 2> /dev/null
-mv -v .pylintrc > $old_dir 2> /dev/null
-mv -v .style.yapf > $old_dir 2> /dev/null
+mv -v .bash_aliases $old_dir 2> /dev/null
+mv -v .bashrc $old_dir 2> /dev/null
+mv -v .gitconfig $old_dir 2> /dev/null
+mv -v .inputrc $old_dir 2> /dev/null
+mv -v .mdlrc $old_dir 2> /dev/null
+mv -v .nanorc $old_dir 2> /dev/null
+mv -v .pre-commit-config.yaml $old_dir 2> /dev/null
+mv -v .pylintrc $old_dir 2> /dev/null
+mv -v .style.yapf $old_dir 2> /dev/null
 
 # Create symlinks in the home folder
 #===================================
@@ -32,3 +32,14 @@ ln -sf $dotfiles_dir/.nanorc ~/.nanorc
 ln -sf $dotfiles_dir/.pre-commit-config.yaml ~/.pre-commit-config.yaml
 ln -sf $dotfiles_dir/.pylintrc ~/.pylintrc
 ln -sf $dotfiles_dir/.style.yapf ~/.style.yapf
+
+
+if [[ $DESKTOP_SESSION == 'plasma' ]]; then
+    mkdir $old_dir/kde
+    mv -v .config/kdeglobals $old_dir/kde 2> /dev/null
+    mv -v .config/kglobalshortcutsrc $old_dir/kde 2> /dev/null
+    mv -v .config/khotkeysrc $old_dir/kde 2> /dev/null
+    ln -sf $dotfiles_dir/kde/kdeglobals .config/kdeglobals
+    ln -sf $dotfiles_dir/kde/kglobalshortcutsrc .config/kglobalshortcutsrc
+    ln -sf $dotfiles_dir/kde/khotkeysrc .config/khotkeysrc
+fi
